d3777261399ff76d4a06fa2695745e4ff81da04f
정기욱 <rldnr56@me.com>
2019-02-16 17:41:58 +0900

    프로그래밍방식으로 만들어져있는 CSButton 클래스를 통한 버튼 표시

--- CustomizingClass/ViewController.swift
+++ CustomizingClass/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let frame = CGRect(x: 30, y: 50, width: 150, height: 30)
+        let csBtn = CSButton(frame: frame)
+        self.view.addSubview(csBtn)
     }
 
 
